a0aa8f5b03cd2c32feb12a9022ab4cd50e0b699f
Guilherme <guilhermenoliani@gmail.com>
2022-05-16 18:09:52 -0300

    Refatoração

---
+++ assets/js/etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    title: "VEREADOR",
+    numbers: 5,
+    candidates: [
+      {
+        number: "38111",
+        name: "Fulano de Tal",
+        party: "ABC",
+        photo: [{ url: "38111.jpg", legenda: "Vereador" }],
+      },
+      {
+        number: "77222",
+        name: "Beltrano da Silva",
+        party: "DEFG",
+        photo: [{ url: "77222.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+  {
+    title: "PREFEITO",
+    numbers: 2,
+    candidates: [
+      {
+        number: "99",
+        name: "Ciclano",
+        party: "ABC",
+        vice: "Cic",
+        photo: [
+          { url: "99.jpg", legend: "Prefeito" },
+          { url: "99_2.jpg", legend: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        number: "84",
+        name: "Zulano",
+        party: "QWERTY",
+        vice: "Zul",
+        photo: [
+          { url: "84.jpg", legend: "Prefeito" },
+          { url: "84_2.jpg", legend: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
