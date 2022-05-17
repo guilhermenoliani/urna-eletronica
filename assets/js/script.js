6f149172baa995433f1d889d19cf6a06f97d92ff
Guilherme <guilhermenoliani@gmail.com>
2022-05-17 13:43:37 -0300

    Selecionando objetos da tela

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//Controlando os objetos da tela
+let voteFor = document.querySelector("#upside--text span"); //Selecionando o texto "Seu voto para".
+let office = document.querySelector("#upside--office span"); // Selecionando o cargo do candidato.
+let voteNumbers = document.querySelector("#upside--number-container"); //Selecionando o local de números.
+let information = document.querySelector("#upside--information"); //Selecionando as informações dos candidatos.
+let candidatesImg = document.querySelector("#upside--right"); // Selecionando imagem dos candidatos.
+let instructions = document.querySelector("#part-down"); //Selecionando instruções da tela da parte de baixo.
